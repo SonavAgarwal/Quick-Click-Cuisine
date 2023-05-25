04b051f272c0415998817af979e4ea795d6460cc
Rahul Ravi <rahulravi1026@gmail.com>
2023-05-25 01:03:15 -0700

    routing features added in order to get started on the sandwich ordering section

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -1,13 +1,22 @@
 import './landing.scss'
 import React from 'react';
 import { InstantOrderCard } from './InstantOrderCard';
+import { useNavigate } from 'react-router-dom';
 
 export function Landing(){
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        //navigate('/sandwich');
+        window.location.href = '/sandwich';
+    };
+
     return <div className="landingPage">
         <div className = "header">
             <div className="mainTitle">Welcome, Rahul</div>
             <div className ="mainSubtitle">The estimated wait time at the Study is <span id = "subtitleBold">69 minutes.</span></div>
             <button className = "mainOrderButton">Order</button>
+            <button className = "mainOrderButton" onClick = {handleClick}>Sandwich Test</button>
         </div>
         <div className="instantOrder">
             <div className = "sectionTitle">Instant Order</div>
