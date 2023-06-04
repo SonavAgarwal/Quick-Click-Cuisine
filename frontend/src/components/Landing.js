418715b0e348fb502bffd3f30957357e2a4cb7eb
Sonav Agarwal <42980809+SonavAgarwal@users.noreply.github.com>
2023-06-04 01:00:36 -0700

    add axios and test get/post requests

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -1,39 +1,74 @@
-import './landing.scss'
-import React from 'react';
-import { InstantOrderCard } from './InstantOrderCard';
-import { PendingOrderCard } from './PendingOrderCard';
-import { HistoryCard } from './HistoryCard';
-import { signOut } from 'firebase/auth';
-import { auth } from '../firebase';
-import { NameText } from './NameText';
+import "./landing.scss";
+import React, { useEffect } from "react";
+import { InstantOrderCard } from "./InstantOrderCard";
+import { PendingOrderCard } from "./PendingOrderCard";
+import { HistoryCard } from "./HistoryCard";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+import { NameText } from "./NameText";
+import axios from "axios";
 
-export function Landing(){
+export function Landing() {
+	useEffect(() => {
+		fetch("http://127.0.0.1:5000/orders/inprogress")
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+			});
+	}, []);
 
-    return <div className="landingPage">
-        <div className = "header">
-            <div className="mainTitle">Welcome, <NameText/></div>
-            <div className ="mainSubtitle">The estimated wait time at the Study is <span id = "subtitleBold">69 minutes.</span></div>
-            <button className = "mainOrderButton" onClick = {() => window.location.href = '/order'}>Order</button>
-            <button className = "mainOrderButton" onClick = {() => signOut(auth)}>Sign out button (Temporary)</button>
-        </div>
-        <div className = "landingContent">
-            <div className="instantOrder">
-                <div className = "sectionTitle">Instant Order</div>
-                <InstantOrderCard type="salad" name = "Ishan Garg 😩"/>
-                <InstantOrderCard type ="pizza" name = "Rahul's Dumpy 🍑"/>
-                <InstantOrderCard type = "sandwich" name = "Sonav 😋" />
-                <InstantOrderCard type = "salad" name = "Willie 🥵" />
-            </div>
-            <div className = "pendingOrders">
-                <div className = "sectionTitle">Pending Orders</div>
-                <PendingOrderCard type = "Sandwich" desc = "Sandwich with bro idk someone help me please oh dear lord" />
-                <PendingOrderCard type = "Pizza" desc = "a pizza bro i really don't know how much longer i can take this at this point"/>
-                <PendingOrderCard type = "Salad" desc = "a salad bro i really don't know how much longer i can take this at this point" />
-            </div>
-            <div className = "orderHistory">
-                <div className = "sectionTitle">Order History</div>
-                <HistoryCard type = "Sandwich" desc = "great sandwich that was eaten like many days ago" date = "4/20/23"/>
-            </div>
-        </div>
-    </div>
+	return (
+		<div className="landingPage">
+			<div className="header">
+				<div className="mainTitle">
+					Welcome, <NameText />
+				</div>
+				<div className="mainSubtitle">
+					The estimated wait time at the Study is{" "}
+					<span id="subtitleBold">69 minutes.</span>
+				</div>
+				<button
+					className="mainOrderButton"
+					onClick={() => (window.location.href = "/order")}
+				>
+					Order
+				</button>
+				<button className="mainOrderButton" onClick={() => signOut(auth)}>
+					Sign out button (Temporary)
+				</button>
+			</div>
+			<div className="landingContent">
+				<div className="instantOrder">
+					<div className="sectionTitle">Instant Order</div>
+					<InstantOrderCard type="salad" name="Ishan Garg 😩" />
+					<InstantOrderCard type="pizza" name="Rahul's Dumpy 🍑" />
+					<InstantOrderCard type="sandwich" name="Sonav 😋" />
+					<InstantOrderCard type="salad" name="Willie 🥵" />
+				</div>
+				<div className="pendingOrders">
+					<div className="sectionTitle">Pending Orders</div>
+					<PendingOrderCard
+						type="Sandwich"
+						desc="Sandwich with bro idk someone help me please oh dear lord"
+					/>
+					<PendingOrderCard
+						type="Pizza"
+						desc="a pizza bro i really don't know how much longer i can take this at this point"
+					/>
+					<PendingOrderCard
+						type="Salad"
+						desc="a salad bro i really don't know how much longer i can take this at this point"
+					/>
+				</div>
+				<div className="orderHistory">
+					<div className="sectionTitle">Order History</div>
+					<HistoryCard
+						type="Sandwich"
+						desc="great sandwich that was eaten like many days ago"
+						date="4/20/23"
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
