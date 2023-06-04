8e2acc9468b05b3f2cc6d6dfcf4aaab48c9cb16c
Ishan Garg <ishangarg@gmail.com>
2023-06-03 22:57:06 -0700

    Add order timestamps and retrieve all orders
    - Added a timestamp to placing orders so that they can later be sorted and processed for wait times
    - Add a route to retrieve all of the orders that have ever been processed by the backend

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 import requests
+from datetime import datetime
 from helpers import generate_id
 
 app = Flask(__name__)
@@ -20,6 +21,7 @@ def create_order():
         'ingredients': data.get('ingredients'),
         'type': data.get('type'),
         'status': 'in progress',
+        'timestamp': datetime.utcnow()  # add timestamp when the order is created
     }
     mongo.db.orders.insert_one(order)
     return jsonify({'message': 'Order placed successfully', 'order_id': order_id}), 201
@@ -42,5 +44,13 @@ def get_past_orders(user_id):
     except Exception as e:
         return jsonify({'error': 'Database error', 'message': str(e)}), 500
 
+@app.route('/orders/all', methods=['GET'])
+def get_all_orders():
+    try:
+        all_orders = mongo.db.orders.find()
+        return jsonify(list(all_orders)), 200
+    except Exception as e:
+        return jsonify({'error': 'Database error', 'message': str(e)}), 500
+    
 if __name__ == '__main__':
     app.run(debug=True)
