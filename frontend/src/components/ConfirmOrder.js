35f6ca5f5ccbd6bff2f4b7c8580394feb6496f3b
Sonav Agarwal <42980809+SonavAgarwal@users.noreply.github.com>
2023-06-04 01:10:12 -0700

    add place order code

--- frontend/src/components/ConfirmOrder.js
+++ frontend/src/components/ConfirmOrder.js
@@ -1,143 +1,235 @@
-import './sandwich.scss'
-import { React } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { breadSandwichData, cheeseSandwichData, toppingsSandwichData, addOnsSandwichData, spreadsCondimentsSandwichData, sidesData, beverageData } from './sandwichIngredientsData';
-import { saucePizzaData, cheesePizzaData, toppingsPizzaData, addOnsPizzaData } from './pizzaIngredientsData';
-import { greensSaladData, proteinsSaladData, toppingsSaladData, dressingsSaladData } from './saladIngredientsData';
-import { NameText } from './NameText';
+import "./sandwich.scss";
+import { React } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import {
+	breadSandwichData,
+	cheeseSandwichData,
+	toppingsSandwichData,
+	addOnsSandwichData,
+	spreadsCondimentsSandwichData,
+	sidesData,
+	beverageData,
+} from "./sandwichIngredientsData";
+import {
+	saucePizzaData,
+	cheesePizzaData,
+	toppingsPizzaData,
+	addOnsPizzaData,
+} from "./pizzaIngredientsData";
+import {
+	greensSaladData,
+	proteinsSaladData,
+	toppingsSaladData,
+	dressingsSaladData,
+} from "./saladIngredientsData";
+import { NameText } from "./NameText";
+import axios from "axios";
 
 export function ConfirmOrder() {
-    const location = useLocation();
-    const fromPage = location.state.fromPage;
-    const type = location.state.type;
-    const finalOrder = location.state.finalOrder || [];
+	const location = useLocation();
+	const fromPage = location.state.fromPage;
+	const type = location.state.type;
+	const finalOrder = location.state.finalOrder || [];
 
-    let firstId, firstData, firstOptionalText;
-    let secondId, secondData, secondOptionalText;
-    let thirdId, thirdData, thirdOptionalText;
-    let fourthId, fourthData, fourthOptionalText;
-    let fifthId, fifthItem, fifthName, fifthOptionalText;
+	let firstId, firstData, firstOptionalText;
+	let secondId, secondData, secondOptionalText;
+	let thirdId, thirdData, thirdOptionalText;
+	let fourthId, fourthData, fourthOptionalText;
+	let fifthId, fifthItem, fifthName, fifthOptionalText;
 
-    if(type === "sandwich" || (type === "sandwich" && fromPage === "confirmOrder")) {
-        firstId = finalOrder.ingredients.ingredients.bread;
-        firstData = breadSandwichData;
-        firstOptionalText = "No Bread";
+	if (
+		type === "sandwich" ||
+		(type === "sandwich" && fromPage === "confirmOrder")
+	) {
+		firstId = finalOrder.ingredients.ingredients.bread;
+		firstData = breadSandwichData;
+		firstOptionalText = "No Bread";
 
-        secondId = finalOrder.ingredients.ingredients.cheese;
-        secondData = cheeseSandwichData;
-        secondOptionalText = "No Cheese"
+		secondId = finalOrder.ingredients.ingredients.cheese;
+		secondData = cheeseSandwichData;
+		secondOptionalText = "No Cheese";
 
-        thirdId = finalOrder.ingredients.ingredients.toppings;
-        thirdData = toppingsSandwichData;
-        thirdOptionalText = "No Toppings";
+		thirdId = finalOrder.ingredients.ingredients.toppings;
+		thirdData = toppingsSandwichData;
+		thirdOptionalText = "No Toppings";
 
-        fourthId = finalOrder.ingredients.ingredients.addOns;
-        fourthData = addOnsSandwichData;
-        fourthOptionalText = "No Add Ons";
+		fourthId = finalOrder.ingredients.ingredients.addOns;
+		fourthData = addOnsSandwichData;
+		fourthOptionalText = "No Add Ons";
 
-        fifthId = finalOrder.ingredients.ingredients.spreadsCondiments;
-        fifthItem = fifthId.flatMap(id => spreadsCondimentsSandwichData.find(item => item.id === id));
-        fifthOptionalText = "No Spreads or Condiments";
-        fifthName = (fifthItem.length !== 0) ? fifthItem.map(item => item.name) : [fifthOptionalText];
-    }
-    else if(type === "pizza" || (type === "pizza" && fromPage === "confirmOrder")) {
-        firstId = finalOrder.ingredients.ingredients.sauce;
-        firstData = saucePizzaData;
-        firstOptionalText = "No Sauce";
+		fifthId = finalOrder.ingredients.ingredients.spreadsCondiments;
+		fifthItem = fifthId.flatMap((id) =>
+			spreadsCondimentsSandwichData.find((item) => item.id === id)
+		);
+		fifthOptionalText = "No Spreads or Condiments";
+		fifthName =
+			fifthItem.length !== 0
+				? fifthItem.map((item) => item.name)
+				: [fifthOptionalText];
+	} else if (
+		type === "pizza" ||
+		(type === "pizza" && fromPage === "confirmOrder")
+	) {
+		firstId = finalOrder.ingredients.ingredients.sauce;
+		firstData = saucePizzaData;
+		firstOptionalText = "No Sauce";
 
-        secondId = finalOrder.ingredients.ingredients.cheese;
-        secondData = cheesePizzaData;
-        secondOptionalText = "No Cheese";
+		secondId = finalOrder.ingredients.ingredients.cheese;
+		secondData = cheesePizzaData;
+		secondOptionalText = "No Cheese";
 
-        thirdId = finalOrder.ingredients.ingredients.toppings;
-        thirdData = toppingsPizzaData;
-        thirdOptionalText = "No Toppings";
+		thirdId = finalOrder.ingredients.ingredients.toppings;
+		thirdData = toppingsPizzaData;
+		thirdOptionalText = "No Toppings";
 
-        fourthId = finalOrder.ingredients.ingredients.addOns;
-        fourthData = addOnsPizzaData;
-        fourthOptionalText = "No Add Ons";
-    }
-    else if(type === "salad" || (type === "salad" && fromPage === "confirmOrder")) {
-        firstId = finalOrder.ingredients.ingredients.green;
-        firstData = greensSaladData;
-        firstOptionalText = "No Greens";
+		fourthId = finalOrder.ingredients.ingredients.addOns;
+		fourthData = addOnsPizzaData;
+		fourthOptionalText = "No Add Ons";
+	} else if (
+		type === "salad" ||
+		(type === "salad" && fromPage === "confirmOrder")
+	) {
+		firstId = finalOrder.ingredients.ingredients.green;
+		firstData = greensSaladData;
+		firstOptionalText = "No Greens";
 
-        secondId = finalOrder.ingredients.ingredients.protein;
-        secondData = proteinsSaladData;
-        secondOptionalText = "No Protein";
+		secondId = finalOrder.ingredients.ingredients.protein;
+		secondData = proteinsSaladData;
+		secondOptionalText = "No Protein";
 
-        thirdId = finalOrder.ingredients.ingredients.toppings;
-        thirdData = toppingsSaladData;
-        thirdOptionalText = "No Toppings";
+		thirdId = finalOrder.ingredients.ingredients.toppings;
+		thirdData = toppingsSaladData;
+		thirdOptionalText = "No Toppings";
 
-        fourthId = finalOrder.ingredients.ingredients.dressing;
-        fourthData = dressingsSaladData;
-        thirdOptionalText = "No Dressing";
-    }
+		fourthId = finalOrder.ingredients.ingredients.dressing;
+		fourthData = dressingsSaladData;
+		thirdOptionalText = "No Dressing";
+	}
 
-    const firstItem = firstId.flatMap(id => firstData.find(item => item.id === id));
-    const firstName = (firstItem.length !== 0) ? firstItem.map(item => item.name) : [firstOptionalText];
+	const firstItem = firstId.flatMap((id) =>
+		firstData.find((item) => item.id === id)
+	);
+	const firstName =
+		firstItem.length !== 0
+			? firstItem.map((item) => item.name)
+			: [firstOptionalText];
 
-    const secondItem = secondId.flatMap(id => secondData.find(item => item.id === id));
-    const secondName = (secondItem.length !== 0) ? secondItem.map(item => item.name) : [secondOptionalText];
+	const secondItem = secondId.flatMap((id) =>
+		secondData.find((item) => item.id === id)
+	);
+	const secondName =
+		secondItem.length !== 0
+			? secondItem.map((item) => item.name)
+			: [secondOptionalText];
 
-    const thirdItem = thirdId.flatMap(id => thirdData.find(item => item.id === id));
-    const thirdName = (thirdItem.length !== 0) ? thirdItem.map(item => item.name) : [thirdOptionalText];
+	const thirdItem = thirdId.flatMap((id) =>
+		thirdData.find((item) => item.id === id)
+	);
+	const thirdName =
+		thirdItem.length !== 0
+			? thirdItem.map((item) => item.name)
+			: [thirdOptionalText];
 
-    const fourthItem = fourthId.flatMap(id => fourthData.find(item => item.id === id));
-    const fourthName = (fourthItem.length !== 0) ? fourthItem.map(item => item.name) : [fourthOptionalText];
+	const fourthItem = fourthId.flatMap((id) =>
+		fourthData.find((item) => item.id === id)
+	);
+	const fourthName =
+		fourthItem.length !== 0
+			? fourthItem.map((item) => item.name)
+			: [fourthOptionalText];
 
-    const sideId = finalOrder.side[0];
-    const sideItem = sidesData.find(item => item.id === sideId);
-    const sideName = sideItem ? sideItem.name : 'No Side';
+	const sideId = finalOrder.side[0];
+	const sideItem = sidesData.find((item) => item.id === sideId);
+	const sideName = sideItem ? sideItem.name : "No Side";
 
-    const beverageId = finalOrder.beverage[0];
-    const beverageItem = beverageData.find(item => item.id === beverageId);
-    const beverageName = beverageItem ? beverageItem.name : 'No Beverage';
+	const beverageId = finalOrder.beverage[0];
+	const beverageItem = beverageData.find((item) => item.id === beverageId);
+	const beverageName = beverageItem ? beverageItem.name : "No Beverage";
 
-    const navigate = useNavigate();
-    
-    const handleEditOrder = () => {
-        const destination = '/' + type;
-        navigate(destination, {state: { finalOrder, fromPage : "confirmOrder" }});
-        //window.location.href = destination;
-        //const destination = `/sandwich/${finalOrder}`;
-        //const state = { finalOrder };
-        //navigate('/sandwich', { state });
-    };
+	const navigate = useNavigate();
 
-    const handlePlaceOrder = () => {
-        window.location.href = '/';
-    };
+	const handleEditOrder = () => {
+		const destination = "/" + type;
+		navigate(destination, { state: { finalOrder, fromPage: "confirmOrder" } });
+		//window.location.href = destination;
+		//const destination = `/sandwich/${finalOrder}`;
+		//const state = { finalOrder };
+		//navigate('/sandwich', { state });
+	};
 
-    return <div className="sandwichPage">
-        <div className = "header">
-            <div className = "mainTitle"><NameText/>, Confirm Your Order!</div>
-            <div className = "mainSubtitle">Make sure that your order is correct.</div>
-            <div className = "ingredientType">
-                {/* <div className = "finalOrderText">{firstName} {firstOptionalText}</div> */}
-                {firstName.map((name, index) => (
-                    <div key={index} className = "finalOrderText">{name}</div>
-                ))}
-                {secondName.map((name, index) => (
-                    <div key={index} className = "finalOrderText">{name}</div>
-                ))}
-                {thirdName.map((name, index) => (
-                    <div key={index} className = "finalOrderText">{name}</div>
-                ))}
-                {fourthName.map((name, index) => (
-                    <div key={index} className = "finalOrderText">{name}</div>
-                ))}
-                {(type === "sandwich" || (type === "sandwich" && fromPage === "confirmOrder")) && (fifthName.map((name, index) => (
-                    <div key={index} className = "finalOrderText">{name}</div>
-                )))}
-                <div className = "finalOrderText">{sideName}</div>
-                <div className = "finalOrderText">{beverageName}</div>
-            </div>
-            <div className = "confirmButtons">
-                <button className = "confirmOrderButton" onClick={handleEditOrder}>Edit Order</button>
-                <button className = "confirmOrderButton" onClick={handlePlaceOrder}>Place Order</button>
-            </div>
-        </div>
-    </div>
+	async function sendPostRequest() {
+		const data = {
+			user_id: 123,
+			ingredients: ["bread", "cheese", "ham"],
+			type: "sandwich",
+		};
+
+		const response = await axios.post("http://127.0.0.1:5000/order", data);
+
+		if (response.status === 201) {
+			console.log("Order placed successfully!");
+			console.log(response.data);
+		} else {
+			console.log("Error placing order!");
+			console.log(response.data);
+		}
+	}
+
+	const handlePlaceOrder = () => {
+		// window.location.href = "/";
+		sendPostRequest();
+	};
+
+	return (
+		<div className="sandwichPage">
+			<div className="header">
+				<div className="mainTitle">
+					<NameText />, Confirm Your Order!
+				</div>
+				<div className="mainSubtitle">
+					Make sure that your order is correct.
+				</div>
+				<div className="ingredientType">
+					{/* <div className = "finalOrderText">{firstName} {firstOptionalText}</div> */}
+					{firstName.map((name, index) => (
+						<div key={index} className="finalOrderText">
+							{name}
+						</div>
+					))}
+					{secondName.map((name, index) => (
+						<div key={index} className="finalOrderText">
+							{name}
+						</div>
+					))}
+					{thirdName.map((name, index) => (
+						<div key={index} className="finalOrderText">
+							{name}
+						</div>
+					))}
+					{fourthName.map((name, index) => (
+						<div key={index} className="finalOrderText">
+							{name}
+						</div>
+					))}
+					{(type === "sandwich" ||
+						(type === "sandwich" && fromPage === "confirmOrder")) &&
+						fifthName.map((name, index) => (
+							<div key={index} className="finalOrderText">
+								{name}
+							</div>
+						))}
+					<div className="finalOrderText">{sideName}</div>
+					<div className="finalOrderText">{beverageName}</div>
+				</div>
+				<div className="confirmButtons">
+					<button className="confirmOrderButton" onClick={handleEditOrder}>
+						Edit Order
+					</button>
+					<button className="confirmOrderButton" onClick={handlePlaceOrder}>
+						Place Order
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 }
--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -1,5 +1,5 @@
 import "./landing.scss";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { InstantOrderCard } from "./InstantOrderCard";
 import { PendingOrderCard } from "./PendingOrderCard";
 import { HistoryCard } from "./HistoryCard";
@@ -9,13 +9,33 @@ import { NameText } from "./NameText";
 import axios from "axios";
 
 export function Landing() {
+	const [estimatedTime, setEstimatedTime] = useState("Loading");
+
 	useEffect(() => {
 		fetch("http://127.0.0.1:5000/orders/inprogress")
 			.then((res) => res.json())
 			.then((data) => {
-				console.log(data);
+				setEstimatedTime(data?.length * 5);
 			});
 	}, []);
+
+	async function sendPostRequest() {
+		const data = {
+			user_id: 123,
+			ingredients: ["bread", "cheese", "ham"],
+			type: "sandwich",
+		};
+
+		const response = await axios.post("http://127.0.0.1:5000/order", data);
+
+		if (response.status === 201) {
+			console.log("Order placed successfully!");
+			console.log(response.data);
+		} else {
+			console.log("Error placing order!");
+			console.log(response.data);
+		}
+	}
 
 	return (
 		<div className="landingPage">
@@ -25,7 +45,7 @@ export function Landing() {
 				</div>
 				<div className="mainSubtitle">
 					The estimated wait time at the Study is{" "}
-					<span id="subtitleBold">69 minutes.</span>
+					<span id="subtitleBold">{estimatedTime} minutes.</span>
 				</div>
 				<button
 					className="mainOrderButton"
@@ -35,6 +55,14 @@ export function Landing() {
 				</button>
 				<button className="mainOrderButton" onClick={() => signOut(auth)}>
 					Sign out button (Temporary)
+				</button>
+				<button
+					className="mainOrderButton"
+					onClick={function () {
+						sendPostRequest();
+					}}
+				>
+					Send post request
 				</button>
 			</div>
 			<div className="landingContent">
