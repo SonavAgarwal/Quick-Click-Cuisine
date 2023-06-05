5a5acb194da4ca3b7405a6ac16fec5031e76fa7f
priyanshush325 <priyanshush325@gmail.com>
2023-06-04 22:43:23 -0700

    fixed edit icon for order history title

--- frontend/src/components/HistoryCard.js
+++ frontend/src/components/HistoryCard.js
@@ -60,7 +60,8 @@ export const HistoryCard = (props) => {
                 <div className = "textContainer">
                     <div className = "orderContainer">
                         <input className = "orderTitle" id = "inputField" defaultValue={orderTitle} maxLength={10} onClick={handleTitleClick} onBlur={handleTitleBlur} onChange={handleNameChange} onKeyDown = {handleKeyDown}></input>
-                        {!isEditing && <img className = "writeIcon" src = {writeIcon} onClick={redirectToInputField}></img>}
+                        {/* {!isEditing && <img className = "writeIcon" src = {writeIcon} onClick={redirectToInputField}></img>} */}
+                        <div>✏️</div>
                     </div>
                     <div className = "date">{date}</div>
                     <div className = "orderDesc">{desc}</div>
