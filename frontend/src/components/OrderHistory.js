3d443c1e9bf55b9e6ba2e4a882a939d87d2d00c1
priyanshush325 <priyanshush325@gmail.com>
2023-06-06 18:42:47 -0700

    added ingredient searching functionality to order history

--- frontend/src/components/OrderHistory.js
+++ frontend/src/components/OrderHistory.js
@@ -6,51 +6,48 @@ import axios from "axios";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../firebase";
 import searchIcon from '../static/search.png';
-import magnifyingGlass from '../static/magnifying-glass.png';
+import magnifyingGlass from '../static/magnifying-glass.png'
 import { HistoryCard } from "./HistoryCard";
 
 export function OrderHistory() {
-    const [user] = useAuthState(auth);
-    const navigate = useNavigate();
+	const [user] = useAuthState(auth);
+	const navigate = useNavigate();
 
-    const [orderHistory, setOrderHistory] = useState([]);
-    const [filteredHistory, setFilteredHistory] = useState([]);
     const [searchQuery, setSearchQuery] = useState("");
+    const [displayedOrders, setDisplayedOrders] = useState([]);
 
-    function fetchOrderHistory() {
+
+    useEffect(() => {
+        fetchOrders();
+    }, [])
+    
+
+    const fetchOrders = () => {
         const user_id = user?.uid;
-        fetch("http://127.0.0.1:5000/orders/past/user/" + user_id)
+        if (searchQuery === ""){
+            fetch("http://127.0.0.1:5000/orders/past/user/" + user_id)
+                .then((res) => res.json())
+                .then((data) => {
+                    setDisplayedOrders(data);
+                });
+        }
+        else{
+            fetch("http://127.0.0.1:5000/user/" + user_id + "/searchIngredients/" + searchQuery)
             .then((res) => res.json())
             .then((data) => {
-                setOrderHistory(data);
-                setFilteredHistory(data);
+                setDisplayedOrders(data);
             });
+        }
     }
-
-    useEffect(() => {
-        setFilteredHistory(orderHistory);
-    }, []);
-
-    useEffect(() => {
-        handleSearch();
-      }, [searchQuery]);
 
     const handleSearchChange = (event) => {
         setSearchQuery(event.target.value);
     };
 
     const handleSearch = () => {
-        const lowercaseQuery = searchQuery.toLowerCase();
-
-        const temp = orderHistory.filter((order) => {
-            const parsed = JSON.parse(order);
-            const ingredients = parsed.ingredients?.map((ingredient) => ingredient.toLowerCase());
-            const type = parsed.type.toLowerCase();
-            return ingredients?.some((ingredient) => ingredient.includes(lowercaseQuery)) ||
-                   type === lowercaseQuery;
-        });
-      
-        setFilteredHistory(temp);
+        // const temp = searchQuery + " i am just added here for fun";
+        // setSearchQuery(temp);
+        fetchOrders();
     };
 
     const handleKeyDown = (event) => {
@@ -60,36 +57,31 @@ export function OrderHistory() {
         }
      };
 
-     useEffect(() => {
-        if (!user) return;
-        fetchOrderHistory();
-    }, [user]);
-
-    return (
-        <div className="orderHistory">
-            <div className="header">
-                <div className="mainTitle">
-                    <NameText />, Here's Your Order History!
-                </div>
-                <div className="mainSubtitle">
-                    View all of your past orders here, and add them to your instant orders as needed.
-                </div>
-                <div className="buttons">
-                    <button className="button" onClick={() => (window.location.href = "/")}>
-                        Home
-                    </button>
-                </div>
+	return (
+		<div className="orderHistory">
+			<div className="header">
+				<div className="mainTitle">
+					<NameText />, Here's Your Order History!
+				</div>
+				<div className="mainSubtitle">
+					View all of your past orders here, and add them to your instant orders as needed.
+				</div>
+				<div className="buttons">
+					<button className="button" onClick={() => (window.location.href = "/")}>
+						Home
+					</button>
+				</div>
                 <div className="searchBar">
-                    <input type="text" placeholder="Search for an ingredient or order type..." value={searchQuery} onChange={handleSearchChange} onKeyDown={handleKeyDown}/>
-                    <button onClick={handleSearch}>
+                    <input type="text" placeholder="Search for an ingredient..." value={searchQuery} onChange = {handleSearchChange} onKeyDown={handleKeyDown}/>
+                    <button onClick={fetchOrders}>
                         <img className = "searchIcon" src = {magnifyingGlass}></img>
                     </button>
                 </div>
-            </div>
+			</div>
             <div className="landingContent">
             <div className="orderHistory">
                     <div className="cardContainer">
-                    {filteredHistory?.map((order, index) => {
+                    {displayedOrders?.map((order, index) => {
                         const parsed = JSON.parse(order);
                         const type = parsed.type;
                         const ingredients = parsed.ingredients;
@@ -113,6 +105,6 @@ export function OrderHistory() {
                     </div>
                 </div>
                 </div>
-        </div>
-    );
+		</div>
+	);
 }
