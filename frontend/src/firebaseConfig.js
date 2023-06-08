83824cfac2dfe56c258c7c2461a446f3b36fe49c
Sonav Agarwal <42980809+SonavAgarwal@users.noreply.github.com>
2023-06-07 18:04:31 -0700

    add back firebase config

---
+++ frontend/src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+	apiKey: "AIzaSyAJ2e07Lr9hk9Pr6QBscfEmlaO6M3JDk_w",
+	authDomain: "quick-click-cuisine.firebaseapp.com",
+	projectId: "quick-click-cuisine",
+	storageBucket: "quick-click-cuisine.appspot.com",
+	messagingSenderId: "787235076493",
+	appId: "1:787235076493:web:b38ad6ca1e5f1a5403a5c4",
+};
